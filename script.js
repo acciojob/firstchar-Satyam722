039de71a0b69c7ad6a810e92e61d2a5c26aeb75e
Satyam <nikamsatyam187@gmail.com>
2025-11-13 17:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function firstChar(text) {
-  // your code here
-	for (let i = 0; i < text.length; i++) {
-		if(text.charAt[i]!==' '){
-			return charAt[i];
-}
-	}
+  // Loop through each character of the input string
+  for (let i = 0; i < text.length; i++) {
+    // Check if the current character is not a space
+    if (text[i] !== ' ') {
+      return text[i]; // Return the first non-space character
+    }
+  }
+  // If no non-space character is found, return an empty string
+  return '';
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
